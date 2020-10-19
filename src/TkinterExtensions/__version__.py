5a0e01f7f2de595174b12a9c70924af891c10ade
Tyler Stegmaier <jakar510@gmail.com>
2020-10-19 11:06:41 -0500

    updates / bug fixes

--- src/TkinterExtensions/__version__.py
+++ src/TkinterExtensions/__version__.py
@@ -5,6 +5,6 @@
 # ------------------------------------------------------------------------------
 
 # major.minor[.maintenance]
-VERSION = (1, 4, 31)
+VERSION = (1, 4, 32)
 
 __version__ = version = '.'.join(map(str, VERSION))
